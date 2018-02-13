40b29ff939bc5e55a4df7169807cbdb670c7addf
Alexandre <alex.morad12@hotmail.com>
2018-02-13 16:58:00 -0500

    Added Apollo, GraphQL & server/

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@
  */
 
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Header from './components/Header';
 import TabNavigator from './components/TabNavigator';
 import LinkList from './components/LinkList';
 
+const httpLink = new HttpLink({ uri: 'http://localhost:4000'});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
-        <TabNavigator />
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Header />
+          <TabNavigator />
+        </View>
+      </ApolloProvider>
     );
   }
 }
